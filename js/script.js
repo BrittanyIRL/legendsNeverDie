d0b818b08ea051fef7434d19c5cab644bca6767b
Brittany Brassell <brittany.brassell@gmail.com>
2016-05-09 09:32:36 -0700

    arrows triggers and window following.

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
     };
 
     /// key detection (better to use addEventListener, but this will do)
+    // onkeyup prevents continual motion
     document.body.onkeyup = 
     document.body.onkeydown = function(e){
       if (e.preventDefault) { 
@@ -28,34 +29,66 @@ $(document).ready(function(){
       keys[kc] = e.type == 'keydown';
     };
 
+    var currentX, currentY; 
+
     /// character movement update
     var moveCharacter = function(dx, dy){
       character.x += (dx||0) * character.speedMultiplier;
       character.y += (dy||0) * character.speedMultiplier;
-      character.element.style.left = character.x + 'px';
-      character.element.style.top = character.y + 'px';
+      currentX = character.x + 'px';
+      currentY = character.y + 'px';
+      character.element.style.left = currentX;
+      character.element.style.top = currentY;
+      console.log(currentX + currentY);
+
     };
 
 
     /// character control
     var detectCharacterMovement = function(){
       if ( keys[keys.LEFT] ) {
-        moveCharacter(-1, 0);
+        // moveCharacter(-1, 0);
+        // moveWindow();
+        console.log("you can't go back");
       }
       if ( keys[keys.RIGHT] ) {
         moveCharacter(1, 0);
+        moveWindow();
+        detectGate();
       }
       if ( keys[keys.UP] ) {
         moveCharacter(0, -1);
+        moveWindow();
+
       }
       if ( keys[keys.DOWN] ) {
         moveCharacter(0, 1);
+        moveWindow();
       }
 
     };
 
+    var detectGate = function(){
+    	if (character.x > 1400){
+    		console.log('reached');
+    		}
+    	if (character.x > 1500){
+    		document.body.onkeydown = function(e){   
+				  e.preventDefault();
+				}
+    	}
+    };
+
+    /// window center follow character 
+    // thanks! http://demos.flesler.com/jquery/scrollTo/
+    var moveWindow = function(){
+  		$('#gameSpace').scrollTo({top:currentY, left:currentX});
+    };
+
     /// update current position on screen
     moveCharacter();
+    //moveWindow();
+    
 
 
 
